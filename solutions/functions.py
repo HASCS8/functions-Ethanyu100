dccf0ed7412522c42383769d56e1754bcaf5b8d3
ethan <ethan.yu@has.hc.edu.tw>
2023-10-27 12:59:12 +0800

    hi

--- solutions/functions.py
+++ solutions/functions.py
@@ -2,13 +2,15 @@ def add(a, b):
     """
     Return the sum of a and b.
     """
-    def power(a.b)
+    return a+b
+
 
 def subtract(a, b):
     """
     Return the result of a minus b.
     """
     
+   return a - b
 
 def multiply(a, b):
     """
@@ -21,13 +23,15 @@ def divide(a, b):
     Return the result of a divided by b.
     If b is zero, return None.
     """
-    
+    result = /b 
+     if b<0 
+     return none 
 
 def power(a, b):
     """
     Return the result of raising a to the power of b.
     """
-    
+    result show 
 
 def reverse_string(s):
     """
